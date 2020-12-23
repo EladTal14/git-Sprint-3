5726da2b65670f9e2b8e7ab0626ac14d2b3b7d73
Basya111 <mail4basy@gmail.com>
2020-12-23 14:45:56 +0200

    check

--- Apps/Note/KeepApp.jsx
+++ Apps/Note/KeepApp.jsx
@@ -1,12 +1,22 @@
 import { AddNote } from "./cmps/AddNote.jsx";
 import { NoteList } from "./cmps/NoteList.jsx";
 
-export function KeepApp() {
-  return <section className="keep-app">
-    <h1>Miss Keep App</h1>
-    <AddNote />
-    <NoteList />
-  </section>
+export class MissEmail extends React.Component {
+
+  componentDidMount() {
+    console.log('Page is ready');
+    this.loadNotes()
+
+}
+
+  render() {
+    return <section className="keep-app">
+      <h1>Miss Keep App</h1>
+      <AddNote />
+      <NoteList />
+    </section>
+
+  }
 }
 
 
