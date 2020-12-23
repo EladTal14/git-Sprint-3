a8e17f2248720d8eb68e6f6146f9ad5d5b9fc0c0
Elad Tal <eladtal14@gmail.com>
2020-12-23 13:45:20 +0200

    skeleton service

---
+++ Apps/Mail/services/emailService.js
@@ -0,0 +1,40 @@
+import { utilService } from '../../../services/utilService.js'
+import { storageService } from '../../../services/storage-service.js'
+
+export const emailService = {
+  query
+}
+
+
+const emails;
+const KEY = 'emailDB'
+
+_createEmails()
+
+function _createEmails() {
+  emails = storageService.loadFromStorage(KEY);
+  if (!emails || !emails.length) {
+    // Nothing in localStorage, use demo data
+    emails = _demoEmails();
+    _saveEmailsToStorage();
+  }
+}
+
+function query() {
+  return Promise.resolve(emails)
+}
+
+function _demoEmails() {
+  const emails = [
+    { id: utilService.makeId(), subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1551133930594 },
+    { id: utilService.makeId(), subject: 'hello dear alan', body: 'have you being eating well ', isRead: false, sentAt: 1551133930594 },
+    { id: utilService.makeId(), subject: '1 new job for full stack developer', body: 'come work for us at wix', isRead: false, sentAt: 1563133930594 },
+    { id: utilService.makeId(), subject: 'Come Back to Instagram', body: 'we miss you at insta come look at new pics of your friends', isRead: false, sentAt: 1561133930594 },
+    { id: utilService.makeId(), subject: '1 new job for full stack developer', body: 'come work for us at wix', isRead: false, sentAt: 1581133930594 },
+  ];
+  return emails;
+}
+
+function _saveEmailsToStorage() {
+  storageService.saveToStorage(KEY, emails)
+}
