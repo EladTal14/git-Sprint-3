93c3a3d4a60e964dbbe2b9c841c58e67b4b10216
Basya111 <mail4basy@gmail.com>
2020-12-23 13:56:11 +0200

    changed folder name

---
+++ Apps/Email/EmailApp.jsx
@@ -0,0 +1,13 @@
+import { MissEmail } from "./cmps/MissEmail.jsx";
+
+export class MissEmail extends React.Component {
+
+  render() {
+    return <section className="email-app">
+      <h1>Miss Keep App</h1>
+      <MissEmail />
+    </section>
+  }
+}
+
+
