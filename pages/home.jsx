0f89613da54942312e339102388b2e821643abae
Elad Tal <eladtal14@gmail.com>
2020-12-23 08:22:29 +0200

    first commit

---
+++ pages/home.jsx
@@ -0,0 +1,23 @@
+
+
+
+export class Home extends React.Component {
+
+    state = {
+
+    }
+
+
+    componentDidMount() {
+        console.log(this);
+
+    }
+
+    render() {
+        return (
+            <section>
+                <h2>Home Sweet Home</h2>
+            </section>
+        )
+    }
+}
