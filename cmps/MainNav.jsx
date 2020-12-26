e9402cd0ab090b28a709572d209a284ca56cc5ee
Basya111 <mail4basy@gmail.com>
2020-12-26 12:34:16 +0200

    more css

--- cmps/MainNav.jsx
+++ cmps/MainNav.jsx
@@ -6,7 +6,7 @@ export function MainNav() {
 
     <nav className="main-header flex space-between">
       <div className="left-header flex">
-        <img src="./assets/css/apps/keep/img/unicorn.png" alt="" />
+        <img src="./assets/img/unicorn.png" alt="" />
         <h1>Appsus</h1>
       </div>
       <ul className="nav-list clean-list flex">
@@ -14,7 +14,7 @@ export function MainNav() {
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/email">Email</NavLink></li>
         <li><NavLink to="/note">Notes</NavLink></li>
-        <li><NavLink to="/book">Book</NavLink></li>
+        <li><NavLink to="/book">Books</NavLink></li>
 
       </ul>
     </nav>
