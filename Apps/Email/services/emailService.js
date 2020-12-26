15a923e643785d1f87aa65d2c4e1dbeb73b042e0
Elad Tal <eladtal14@gmail.com>
2020-12-26 22:49:00 +0200

    merge

--- Apps/Email/services/emailService.js
+++ Apps/Email/services/emailService.js
@@ -94,26 +94,91 @@ function _demoEmails() {
   const emails = [
     { sendTo: 'elad', id: utilService.makeId(), subject: 'Wassap?', body: 'Pick up!', isRead: false, sentAt: 1 },
     {
-      sendTo: 'basya', id: utilService.makeId(), subject: 'hello dear alan ', body: `dsfsdj sdlfkj s fjksdj fsdfjlskd fjskldjfksdjfskld jfskldj fsdklfj sdklfjlsdjfskldjfklsdjflsdkjf
-    sdkjfs, seroint where we'd be stuck without them. soundly at night.`, isRead: true, sentAt: 2
+      sendTo: 'basya', id: utilService.makeId(), subject: 'hello dear alan ', body: `Are you coming to work to night?
+    sdkjfs, seroint where we'd be stuck without them. soundly at night.`, isRead: false, sentAt: 2
     },
-    { sendTo: 'yaron', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: false, sentAt: 3 },
-    { sendTo: 'avi', id: utilService.makeId(), subject: 'Come Back to Instagram', body: 'we miss you at insta come look at new pics of your friends', isRead: false, sentAt: 4 },
-    { sendTo: 'Moran', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
+    {
+      sendTo: 'avocado', id: utilService.makeId(), subject: 'Come Back to Instagram', body: `
+    Avocode	Avocode Blog
+    Dear elad,
+    
+    As 2020 is coming to its end, we what to thank you for being with this year and helping us improve Avocode. 🙏
+    
+    What a year! What a year indeed for all of us. We all had to make sacrifices in 2020. We all struggled in different ways dealing with the new reality, and the challenges 2020 threw at us. We can only hope that you, too, were able to cope, make sense of it, and come out stronger on the other side.
+    
+    We took some time to remind ourselves of what we managed to accomplish in 2020 and want to share the highlights with you.
+    
+    See Avocode’s 2020 Review.
+    
+    Happy holidays and all the best to you in 2021! 🥳
+    
+    
+    Avocode Team
+    
+    P. S. Have you noticed something different about your profile picture in Avocode? We hope you enjoy this holiday gift update.`, isRead: false, sentAt: 4
+    },
+    {
+      sendTo: 'Zeplin Crew', id: utilService.makeId(), subject: `Your avocado time is done`, body: `Hi there,
+    
+      Hope you're enjoying Zeplin so far, here are some resources to get the creative juices flowing:
+      
+      Watch a short demo video.
+      Check out quick start articles.
+      Search through frequently asked questions.
+      Our whole crew is always eager to hear any feedback or questions you might have.
+       Ping us anytime by replying or shooting an email to support@zeplin.io.
+      
+      Cheers,
+      Zeplin Crew`, isRead: true, sentAt: 5
+    },
+    {
+      sendTo: 'FaceBook', id: utilService.makeId(), subject: 'Linkedin job alerts', body: `Hi Elad,
+
+    Black Frid‍ay will come and go. But the skills you’ll master will last. Enrolling in one of our Nanodegree programs means you’ll be investing in yourself. Our goal is to make advancing your career within reach, especially with this offer.
+    
+    Our tech courses take a learn by doing approach. Hands-on projects will provide you with the tools you need to get to the next level in your career. You’ll get real-world experience in less than 6‍ mon‍ths as a part-ti‍me student!
+    
+    Enroll n‍ow! Offer ends Dec‍ember 2‍, Wednes‍day at 11‍:59 p‍m!`, isRead: true, sentAt: 5
+    },
+    {
+      sendTo: 'Zeplin Crew', id: utilService.makeId(), subject: `Your Zeplin time is done`, body: `Hi there,
+    
+      Hope you're enjoying Zeplin so far, here are some resources to get the creative juices flowing:
+      
+      Watch a short demo video.
+      Check out quick start articles.
+      Search through frequently asked questions.
+      Our whole crew is always eager to hear any feedback or questions you might have.
+       Ping us anytime by replying or shooting an email to support@zeplin.io.
+      
+      Cheers,
+      Zeplin Crew`, isRead: true, sentAt: 5
+    },
+    { sendTo: 'Aviv', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: false, sentAt: 5 },
     { sendTo: 'FaceBook', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
+    { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: false, sentAt: 5 },
+    { sendTo: 'Aviv', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: false, sentAt: 5 },
+    { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
+    { sendTo: 'Moran', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: false, sentAt: 5 },
     { sendTo: 'Hilel', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'Aviv', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'FaceBook', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'Aviv', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'Moran', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
-    { sendTo: 'Hilel', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
     { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
     { sendTo: 'FaceBook', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
     { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
     { sendTo: 'Moran', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
     { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
+    {
+      sendTo: 'easy.co.il', id: utilService.makeId(), subject: 'הכירו את המומחים של איזי!', body: `ומחי איזי ממליצים
+    חוות דעת מצפון ועד דרום
+    נבחרת המומחים של איזי מסתובבת ברחבי הארץ וכותבת ביקורות עומק על עסקים בישראל, עם ציונים ותמונות מהמקום. קבלו טעימה קטנה ממה שיש למומחים שלנו לומר והחליטו - לבקר או לוותר?
+    
+    
+    אמנות ואימהות
+    
+    מומחית איזי לפעילויות ופנאי
+    
+    אם יוצאים מגלים - גן בוטני ותערוכת פסלים
+    להורים תמיד יש התלבטות מה מתאים לילדים, מה לא יתאים להם, מה ישעמם אותם, מה יעניין אותם, ולא תמיד אפשר לדעת מראש. לפעמים אנחנו חושבים שהילדים שלנו יעופו על משהו ואז מגלים שזה ממש לא מעניין אותם. אני חושבת שהפעילויות הכי מוצלחות הן אלה שלא חושבים עליהן יותר מדי אלא פשוט עושים, לוקחים את הסיכון וקופצים…`, isRead: false, sentAt: 3
+    },
     { sendTo: 'rami', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
     { sendTo: 'FaceBook', id: utilService.makeId(), subject: 'Linkedin job alerts', body: 'come work for us at wix', isRead: true, sentAt: 5 },
   ];
--- pages/About.jsx
+++ pages/About.jsx
@@ -3,7 +3,8 @@ export function About() {
     <section className="about-page">
       <div className="about-container">
         <h1 className="about-header">About</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia iure aliquid inventore assumenda, provident error quas necessitatibus illo, beatae rem at a incidunt, soluta ut dolore sint ipsum voluptatibus magni?</p>
+        <p>"The best project I have ever laid my eyes on"</p>
+        <p>-Yaron Biton</p>
       </div>
     </section>
   )
